292f30767c4c0ef40d299e92a2129a9de41c71c4
John Theo <b_w_ghost@163.com>
2018-10-31 19:13:49 +0800

    redirect homepage to /spacy_parser

--- enr_tag/urls.py
+++ enr_tag/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('spacy_parser/', include('spacyParser.urls')),
     path('api/', include('api.urls')),
